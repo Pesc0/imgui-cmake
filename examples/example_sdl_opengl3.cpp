1784566554e6b459dcd621e7cc95d2d1d967a658
Pesc0 <pesco.conti@gmail.com>
2021-02-16 12:27:33 +0100

    Added raspberry mouse offset fix for the other example

--- examples/example_sdl_opengl3.cpp
+++ examples/example_sdl_opengl3.cpp
@@ -81,6 +81,9 @@ int main(int, char**)
 
 
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    #ifdef __RASPBIAN__
+        window_flags = (SDL_WindowFlags)((int)window_flags | (int)SDL_WINDOW_FULLSCREEN_DESKTOP);
+    #endif
     SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
